e401a08a4f30604fef708ad81aa822371d50554b
xmarek7@fi.muni.cz <xmarek7@fi.muni.cz>
2025-04-02 20:34:23 +0200

    add entity <-> dto mappers

---
+++ band-service/src/main/java/cz/muni/fi/bandmanagementservice/mappers/BandMapper.java
@@ -0,0 +1,34 @@
+package cz.muni.fi.bandmanagementservice.mappers;
+
+import cz.muni.fi.bandmanagementservice.data.model.Band;
+import cz.muni.fi.bandmanagementservice.data.model.BandInfoUpdate;
+import cz.muni.fi.generated.band.model.BandDto;
+import cz.muni.fi.generated.band.model.BandInfoUpdateRequest;
+
+/**
+ * @author Tomáš MAREK
+ */
+public class BandMapper {
+    public static BandDto mapToDto(Band band){
+        BandDto dto = new BandDto();
+        dto.id(band.getId());
+        dto.name(band.getName());
+        dto.musicalStyle(band.getMusicalStyle());
+        dto.managerId(band.getManagerId());
+        dto.logo(band.getLogoUrl());
+        dto.members(band.getMembers().stream().toList());
+        return dto;
+    };
+
+    public static Band mapFromDto(BandDto bandDto){
+        Band band = new Band(bandDto.getId(), bandDto.getName(), bandDto.getMusicalStyle(), bandDto.getManagerId());
+        band.setLogoUrl(bandDto.getLogo().get());
+        band.setMembers(bandDto.getMembers());
+        return band;
+    };
+
+    public static BandInfoUpdate mapFromInfoUpdateRequest(BandInfoUpdateRequest updateRequest){
+        return new BandInfoUpdate(updateRequest.getId(), updateRequest.getName(), updateRequest.getMusicalStyle(), updateRequest.getManagerId(), updateRequest.getLogo().get());
+    };
+
+}
---
+++ band-service/src/main/java/cz/muni/fi/bandmanagementservice/mappers/BandOfferMapper.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.bandmanagementservice.mappers;
+
+import cz.muni.fi.bandmanagementservice.data.model.BandOffer;
+import cz.muni.fi.generated.band.model.BandOfferDto;
+
+/**
+ * @author Tomáš MAREK
+ */public class BandOfferMapper {
+     public static BandOffer mapFromDto(BandOfferDto dto){
+         return new BandOffer(dto.getId(), dto.getBandId(), dto.getInvitedMusicianId(), dto.getOfferingManagerId());
+     }
+
+
+    public static BandOfferDto mapToDto(BandOffer bandOffer){
+         BandOfferDto dto = new BandOfferDto();
+         dto.setId(bandOffer.getId());
+         dto.setBandId(bandOffer.getBandId());
+         dto.setInvitedMusicianId(bandOffer.getInvitedMusicianId());
+         dto.setOfferingManagerId(bandOffer.getOfferingManagerId());
+         return dto;
+    }
+}
