b3db3480c0b8b4728c8ed679c581f70678d0f5d1
xmarek7@fi.muni.cz <xmarek7@fi.muni.cz>
2025-04-03 15:46:14 +0200

    implement ArtistServiceTest

--- user-service/src/test/java/cz/muni/fi/userservice/TestDataFactory.java
+++ user-service/src/test/java/cz/muni/fi/userservice/TestDataFactory.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.userservice;
 
 import cz.muni.fi.userservice.model.Artist;
+import cz.muni.fi.userservice.model.Role;
 
 import java.util.Set;
 
@@ -8,8 +9,30 @@ import java.util.Set;
  * @author Tomáš MAREK
  */
 public class TestDataFactory {
-    public static final Artist TEST_ARTIST_1 = new Artist("Till Lindemann", "Born in 1963, Leipzig", "Singer", Set.of(1L, 2L, 3L));
-    public static final Artist TEST_ARTIST_2 = new Artist("Joakim Brodén", "Born in 1980, Falun", "Singer, Keyboard", Set.of(2L, 4L));
-    public static final Long TEST_ARTIST_1_ID = 1L;
+    public static final Artist TEST_ARTIST_1 = setUpTestArtist1();
+    public static final Artist TEST_ARTIST_2 = setUpTestArtist2();
 
+    private static Artist setUpTestArtist1() {
+        Artist testArtist = new Artist("Till Lindemann", "Born in 1963, Leipzig", "Singer", Set.of(1L, 2L, 3L));
+        testArtist.setId(1L);
+        testArtist.setUsername("xlindemann");
+        testArtist.setEmail("lindemann_till@email.com");
+        testArtist.setFirstName("Till");
+        testArtist.setLastName("Lindemann");
+        testArtist.setRole(Role.ARTIST);
+        testArtist.setPassword("Password in clean? Really guys?");
+        return testArtist;
+    }
+
+    private static Artist setUpTestArtist2() {
+        Artist testArtis = new Artist("Joakim Brodén", "Born in 1980, Falun", "Singer, Keyboard", Set.of(2L, 4L));
+        testArtis.setId(2L);
+        testArtis.setUsername("brodenj");
+        testArtis.setEmail("joakim_broden@example.com");
+        testArtis.setFirstName("Joakim");
+        testArtis.setLastName("Brodén");
+        testArtis.setRole(Role.ARTIST);
+        testArtis.setPassword("Very strong password");
+        return testArtis;
+    }
 }
--- user-service/src/test/java/cz/muni/fi/userservice/service/ArtistServiceTest.java
+++ user-service/src/test/java/cz/muni/fi/userservice/service/ArtistServiceTest.java
@@ -10,9 +10,16 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 
 /**
  * @author Tomáš MAREK
@@ -27,16 +34,195 @@ public class ArtistServiceTest {
     ArtistService artistService;
 
     @Test
-    void findById_artistFound_returnsArtist() {
+    void save_artistSaved_returnsSavedArtist() {
         // Arrange
-        Mockito.when(artistRepository.findById(TestDataFactory.TEST_ARTIST_1_ID)).thenReturn(Optional.of(TestDataFactory.TEST_ARTIST_1));
+        Mockito.when(artistRepository.save(TestDataFactory.TEST_ARTIST_1)).thenReturn(TestDataFactory.TEST_ARTIST_1);
 
         // Act
-        Artist found = artistService.findById(TestDataFactory.TEST_ARTIST_1_ID);
+        Artist saved = artistService.save(TestDataFactory.TEST_ARTIST_1);
+
+        // Assert
+        assertEquals(TestDataFactory.TEST_ARTIST_1, saved);
+    }
+
+    @Test
+    void findById_artistFound_returnsArtist() {
+        // Arrange
+        Mockito.when(artistRepository.findById(TestDataFactory.TEST_ARTIST_1.getId())).thenReturn(Optional.of(TestDataFactory.TEST_ARTIST_1));
+
+        // Act
+        Artist found = artistService.findById(TestDataFactory.TEST_ARTIST_1.getId());
 
         // Assert
         assertEquals(TestDataFactory.TEST_ARTIST_1, found);
     }
 
+    @Test
+    void findById_artistNotFound_returnsNull() {
+        // Arrange
+        Long invalidId = 42L;
+        Mockito.when(artistRepository.findById(invalidId)).thenReturn(Optional.empty());
 
+        // Act
+        Artist found = artistService.findById(invalidId);
+
+        // Assert
+        assertNull(found);
+    }
+
+
+
+    @Test
+    void findByUsername_artistFound_returnsArtist() {
+        // Arrange
+        Mockito.when(artistRepository.findByUsername(TestDataFactory.TEST_ARTIST_1.getUsername())).thenReturn(Optional.of(TestDataFactory.TEST_ARTIST_1));
+
+        // Act
+        Artist found = artistService.findByUsername(TestDataFactory.TEST_ARTIST_1.getUsername());
+
+        // Assert
+        assertEquals(TestDataFactory.TEST_ARTIST_1, found);
+    }
+
+    @Test
+    void findByUsername_artistNotFound_returnsNull() {
+        // Arrange
+        String invalidUsername = "Invalid username";
+        Mockito.when(artistRepository.findByUsername(invalidUsername)).thenReturn(Optional.empty());
+
+        // Act
+        Artist found = artistService.findByUsername(invalidUsername);
+
+        // Assert
+        assertNull(found);
+    }
+
+    @Test
+    void findAll_noArtistsStored_returnsEmptyList() {
+        // Arrange
+        Mockito.when(artistRepository.findAll()).thenReturn(List.of());
+
+        // Act
+        List<Artist> found = artistService.findAll();
+
+        // Assert
+        assertEquals(0, found.size());
+    }
+
+    @Test
+    void findAll_twoArtistsStored_returnsList() {
+        // Arrange
+        Mockito.when(artistRepository.findAll()).thenReturn(List.of(TestDataFactory.TEST_ARTIST_1, TestDataFactory.TEST_ARTIST_2));
+
+        // Act
+        List<Artist> found = artistService.findAll();
+
+        // Assert
+        assertEquals(2, found.size());
+        assertTrue(found.contains(TestDataFactory.TEST_ARTIST_1));
+        assertTrue(found.contains(TestDataFactory.TEST_ARTIST_2));
+    }
+
+    @Test
+    void deleteById_artistPresent_noReturn() {
+        // Act
+        artistService.deleteById(TestDataFactory.TEST_ARTIST_1.getId());
+
+        // Assert
+        Mockito.verify(artistRepository, Mockito.times(1)).deleteById(TestDataFactory.TEST_ARTIST_1.getId());
+    }
+
+    @Test
+    void findByBandIds_noArtisInBands_returnsEmptyList() {
+        // Arrange
+        Set<Long> emptyBandIds = Set.of(5L);
+        Mockito.when(artistRepository.findByBandIds(emptyBandIds)).thenReturn(List.of());
+
+        // Act
+        List<Artist> found = artistService.findByBandIds(emptyBandIds);
+
+        // Assert
+        assertEquals(0, found.size());
+    }
+
+    @Test
+    void findByBandIds_artistsInBands_returnsList() {
+        // Arrange
+        Set<Long> bands = Set.of(2L, 3L);
+        Mockito.when(artistRepository.findByBandIds(bands)).thenReturn(List.of(TestDataFactory.TEST_ARTIST_1, TestDataFactory.TEST_ARTIST_2));
+
+        // Act
+        List<Artist> found = artistService.findByBandIds(bands);
+
+        // Assert
+        assertEquals(2, found.size());
+        assertTrue(found.contains(TestDataFactory.TEST_ARTIST_1));
+        assertTrue(found.contains(TestDataFactory.TEST_ARTIST_2));
+    }
+
+    @Test
+    void findByBandIds_noArtistsInBands_returnsEmptyList() {
+        // Arrange
+        Set<Long> idsOfEmptyBands = Set.of();
+        Mockito.when(artistRepository.findByBandIds(idsOfEmptyBands)).thenReturn(List.of());
+
+        // Act
+        List<Artist> found = artistService.findByBandIds(idsOfEmptyBands);
+
+        // Assert
+        assertEquals(0, found.size());
+    }
+
+    @Test
+    void findByBandIds_emptyBands_returnsEmptyList() {
+        // Arrange
+        Set<Long> emptyBandIds = Set.of();
+        Mockito.when(artistRepository.findByBandIds(emptyBandIds)).thenReturn(List.of());
+
+        // Act
+        List<Artist> found = artistService.findByBandIds(emptyBandIds);
+
+        // Assert
+        assertEquals(0, found.size());
+    }
+
+    @Test
+    void updateArtistByBandsIds_newBandAdded_returnsUpdatedArtist() {
+        // Arrange
+        Mockito.when(artistRepository.findById(TestDataFactory.TEST_ARTIST_1.getId())).thenReturn(Optional.of(TestDataFactory.TEST_ARTIST_1));
+        Mockito.when(artistRepository.save(TestDataFactory.TEST_ARTIST_1)).thenReturn(TestDataFactory.TEST_ARTIST_1);
+        Set<Long> newBandIds = Set.of(1L, 2L, 3L, 4L);
+
+        // Act
+        Artist updated = artistService.updateArtistByBandIds(TestDataFactory.TEST_ARTIST_1.getId(), newBandIds);
+
+        // Assert
+        assertEquals(TestDataFactory.TEST_ARTIST_1, updated);
+        assertEquals(newBandIds, updated.getBandIds());
+    }
+
+    @Test
+    void updateArtistByBandsIds_allBandsRemoved_returnsUpdatedArtist() {
+        // Arrange
+        Mockito.when(artistRepository.findById(TestDataFactory.TEST_ARTIST_1.getId())).thenReturn(Optional.of(TestDataFactory.TEST_ARTIST_1));
+        Mockito.when(artistRepository.save(TestDataFactory.TEST_ARTIST_1)).thenReturn(TestDataFactory.TEST_ARTIST_1);
+        Set<Long> emptyBands = Set.of();
+
+        // Act
+        Artist updated = artistService.updateArtistByBandIds(TestDataFactory.TEST_ARTIST_1.getId(), emptyBands);
+
+        // Assert
+        assertEquals(TestDataFactory.TEST_ARTIST_1, updated);
+        assertEquals(emptyBands, updated.getBandIds());
+    }
+
+    @Test
+    void updateArtistByBandsIds_invalidArtistId_throwsIllegalArgumentException() {
+        // Arrange
+        Long invalidId = 42L;
+        Mockito.when(artistRepository.findById(invalidId)).thenReturn(Optional.empty());
+
+        // Act / Assert
+        assertThrows(IllegalArgumentException.class, () -> artistService.updateArtistByBandIds(invalidId, Set.of(1L, 2L, 3L)));
+    }
 }
