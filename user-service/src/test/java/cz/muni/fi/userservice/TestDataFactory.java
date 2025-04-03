369420fc09b1d088223557b7dcf6d2d686a4ca2d
xmarek7@fi.muni.cz <xmarek7@fi.muni.cz>
2025-04-03 16:22:35 +0200

    add ManagerServiceTest

--- user-service/src/test/java/cz/muni/fi/userservice/TestDataFactory.java
+++ user-service/src/test/java/cz/muni/fi/userservice/TestDataFactory.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.userservice;
 
 import cz.muni.fi.userservice.model.Artist;
+import cz.muni.fi.userservice.model.Manager;
 import cz.muni.fi.userservice.model.Role;
 
 import java.util.Set;
@@ -11,6 +12,37 @@ import java.util.Set;
 public class TestDataFactory {
     public static final Artist TEST_ARTIST_1 = setUpTestArtist1();
     public static final Artist TEST_ARTIST_2 = setUpTestArtist2();
+    public static final Manager TEST_MANAGER_1 = setUpTestManager1();
+    public static final Manager TEST_MANAGER_2 = setUpTestManager2();
+
+
+    private static Manager setUpTestManager1() {
+        Manager testManager = new Manager();
+        testManager.setManagedBandIds(Set.of(1L, 2L, 3L));
+        testManager.setCompanyName("Test Music Management Company");
+        testManager.setId(3L);
+        testManager.setUsername("xdoe7");
+        testManager.setEmail("john_doe@email.com");
+        testManager.setFirstName("John");
+        testManager.setLastName("Doe");
+        testManager.setRole(Role.MANAGER);
+        testManager.setPassword("password123");
+        return testManager;
+    }
+
+    private static Manager setUpTestManager2() {
+        Manager testManager = new Manager();
+        testManager.setManagedBandIds(Set.of(2L, 3L, 4L));
+        testManager.setCompanyName("Managers of Music");
+        testManager.setId(4L);
+        testManager.setUsername("mustermann123");
+        testManager.setEmail("mustermann_m@email.de");
+        testManager.setFirstName("Max");
+        testManager.setLastName("Mustermann");
+        testManager.setRole(Role.MANAGER);
+        testManager.setPassword("MustermannRules!");
+        return testManager;
+    }
 
     private static Artist setUpTestArtist1() {
         Artist testArtist = new Artist("Till Lindemann", "Born in 1963, Leipzig", "Singer", Set.of(1L, 2L, 3L));
