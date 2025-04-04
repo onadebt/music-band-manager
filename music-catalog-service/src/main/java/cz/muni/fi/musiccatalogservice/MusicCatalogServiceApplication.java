69f65fda2014fac9083be75c83f63ae56f03800d
Ivan <567817@muni.cz>
2025-04-05 01:16:12 +0200

    got rid of eureka

--- music-catalog-service/src/main/java/cz/muni/fi/musiccatalogservice/MusicCatalogServiceApplication.java
+++ music-catalog-service/src/main/java/cz/muni/fi/musiccatalogservice/MusicCatalogServiceApplication.java
@@ -2,12 +2,10 @@ package cz.muni.fi.musiccatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EntityScan("cz.muni.fi.musiccatalogservice.model")
 @EnableJpaRepositories("cz.muni.fi.musiccatalogservice.repository")
 public class MusicCatalogServiceApplication {
