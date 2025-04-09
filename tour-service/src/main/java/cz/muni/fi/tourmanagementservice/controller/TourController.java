008621191073fb4619ef821137a6578a846d85ca
Zbyněk Pospíšil <zp.pospisil@seznam.cz>
2025-04-09 11:12:42 +0200

    Adding yaml annotations to Controllers

--- tour-service/src/main/java/cz/muni/fi/tourmanagementservice/controller/TourController.java
+++ tour-service/src/main/java/cz/muni/fi/tourmanagementservice/controller/TourController.java
@@ -3,7 +3,7 @@ package cz.muni.fi.tourmanagementservice.controller;
 
 import cz.muni.fi.tourmanagementservice.dto.TourDTO;
 import cz.muni.fi.tourmanagementservice.facades.TourFacade;
-import cz.muni.fi.tourmanagementservice.service.TourService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,47 +14,58 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/tours")
+@Tag(name = "Tour API", description = "Tour management API")
 public class TourController {
 
     private final TourFacade tourFacade;
 
+    @Autowired
     public TourController(TourFacade tourFacade) {
         this.tourFacade = tourFacade;
     }
 
+    @Operation(summary = "Retrieve all tours")
     @GetMapping
     public ResponseEntity<List<TourDTO>> getAllTours() {
         return ResponseEntity.ok(tourFacade.getAllTours());
     }
 
+    @Operation(summary = "Retrieve tours by band")
     @GetMapping("/band/{bandId}")
     public ResponseEntity<List<TourDTO>> getToursByBand(@PathVariable Long bandId) {
         return ResponseEntity.ok(tourFacade.getToursByBand(bandId));
     }
 
+    @Operation(summary = "Retrieve tours by id")
     @GetMapping("/{id}")
     public ResponseEntity<TourDTO> getTourById(@PathVariable Long id) {
         return ResponseEntity.ok(tourFacade.getTourById(id));
     }
 
+    @Operation(summary = "Create a new tour")
     @PostMapping
     public ResponseEntity<TourDTO> createTour(@RequestBody TourDTO tourDTO) {
         TourDTO createdTour = tourFacade.createTour(tourDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdTour);
     }
 
+    @Operation(summary = "Update a tour")
     @PutMapping("/{id}")
     public ResponseEntity<TourDTO> updateTour(@PathVariable Long id, @RequestBody TourDTO tourDTO) {
         TourDTO updatedTour = tourFacade.updateTour(id, tourDTO);
         return ResponseEntity.ok(updatedTour);
     }
 
+    @Operation(summary = "Delete a tour")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTour(@PathVariable Long id) {
         tourFacade.deleteTour(id);
