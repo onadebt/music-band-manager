85f6f31972c94a809b4cee0da5ec2140af7f6089
Ivan <567817@muni.cz>
2025-04-09 18:07:20 +0200

    updated mappers for User, Manager, Artist

--- user-service/src/main/java/cz/muni/fi/userservice/mappers/ArtistMapper.java
+++ user-service/src/main/java/cz/muni/fi/userservice/mappers/ArtistMapper.java
@@ -1,12 +1,31 @@
 package cz.muni.fi.userservice.mappers;
 
-import cz.muni.fi.userservice.dto.ArtistDTO;
+import cz.muni.fi.userservice.dto.ArtistDto;
+import cz.muni.fi.userservice.dto.ArtistUpdateDto;
 import cz.muni.fi.userservice.model.Artist;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface ArtistMapper {
-    Artist toEntity(ArtistDTO registerDTO);
+    Artist toEntity(ArtistDto registerDTO);
 
-    ArtistDTO toDTO(Artist artist);
+    ArtistDto toDto(Artist artist);
+
+    default Artist updateArtistFromDto(ArtistUpdateDto updateDto, @MappingTarget Artist artist) {
+        UserMapper userMapper = Mappers.getMapper(UserMapper.class);
+        userMapper.updateUserFromDto(updateDto, artist);
+
+        if (updateDto.getStageName() != null) {
+            artist.setStageName(updateDto.getStageName());
+        }
+        if (updateDto.getBio() != null) {
+            artist.setBio(updateDto.getBio());
+        }
+        if (updateDto.getSkills() != null) {
+            artist.setSkills(updateDto.getSkills());
+        }
+        return artist;
+    }
 }
--- user-service/src/main/java/cz/muni/fi/userservice/mappers/ManagerMapper.java
+++ user-service/src/main/java/cz/muni/fi/userservice/mappers/ManagerMapper.java
@@ -1,12 +1,28 @@
 package cz.muni.fi.userservice.mappers;
 
-import cz.muni.fi.userservice.dto.ManagerDTO;
+import cz.muni.fi.userservice.dto.ManagerDto;
+import cz.muni.fi.userservice.dto.ManagerUpdateDto;
 import cz.muni.fi.userservice.model.Manager;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface ManagerMapper {
-    Manager toEntity(ManagerDTO registerDTO);
+    Manager toEntity(ManagerDto registerDTO);
 
-    ManagerDTO toDTO(Manager manager);
+    ManagerDto toDTO(Manager manager);
+
+    default Manager updateManagerFromDto(ManagerUpdateDto updateDto, @MappingTarget Manager manager) {
+        UserMapper userMapper = Mappers.getMapper(UserMapper.class);
+        userMapper.updateUserFromDto(updateDto, manager);
+
+        if (updateDto.getCompanyName() != null) {
+            manager.setCompanyName(updateDto.getCompanyName());
+        }
+        if (updateDto.getManagedBandIds() != null) {
+            manager.setManagedBandIds(updateDto.getManagedBandIds());
+        }
+        return manager;
+    }
 }
