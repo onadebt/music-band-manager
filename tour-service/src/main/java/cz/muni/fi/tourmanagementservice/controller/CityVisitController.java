69b478a8384f60fc1d9b58507af9ddb592326e0c
Zbyněk Pospíšil <zp.pospisil@seznam.cz>
2025-04-09 11:12:42 +0200

    Adding yaml annotations to Controllers

--- tour-service/src/main/java/cz/muni/fi/tourmanagementservice/controller/CityVisitController.java
+++ tour-service/src/main/java/cz/muni/fi/tourmanagementservice/controller/CityVisitController.java
@@ -3,7 +3,8 @@ package cz.muni.fi.tourmanagementservice.controller;
 
 import cz.muni.fi.tourmanagementservice.dto.CityVisitDTO;
 import cz.muni.fi.tourmanagementservice.facades.CityVisitFacade;
-import cz.muni.fi.tourmanagementservice.service.CityVisitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/cityVisits")
+@Tag(name = "CityVisit API", description = "CityVisit management API")
 public class CityVisitController {
 
     private final CityVisitFacade cityVisitFacade;
@@ -29,35 +31,40 @@ public class CityVisitController {
         this.cityVisitFacade = cityVisitFacade;
     }
 
+    @Operation(summary = "Retrieve all city visits")
     @GetMapping
     public ResponseEntity<List<CityVisitDTO>> getAllCityVisits() {
         return ResponseEntity.ok(cityVisitFacade.getAllCityVisits());
     }
 
+    @Operation(summary = "Retrieve all city visits by tour")
     @GetMapping("/tour/{tourId}")
     public ResponseEntity<List<CityVisitDTO>> getCityVisitsByTour(@PathVariable Long tourId) {
         return ResponseEntity.ok(cityVisitFacade.getCityVisitsByTour(tourId));
     }
 
+    @Operation(summary = "Retrieve all city visits by id")
     @GetMapping("/{id}")
     public ResponseEntity<CityVisitDTO> getCityVisitById(@PathVariable Long id) {
         return ResponseEntity.ok(cityVisitFacade.getCityVisitById(id));
     }
 
 
+    @Operation(summary = "Create new city visit")
     @PostMapping
     public ResponseEntity<CityVisitDTO> createCityVisit(@RequestBody CityVisitDTO cityVisitDTO) {
         CityVisitDTO createdCityVisit = cityVisitFacade.createCityVisit(cityVisitDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCityVisit);
     }
 
+    @Operation(summary = "Update a city visit")
     @PutMapping("/{id}")
     public ResponseEntity<CityVisitDTO> updateCityVisit(@PathVariable Long id, @RequestBody CityVisitDTO cityVisitDTO) {
         CityVisitDTO updatedCityVisit = cityVisitFacade.updateCityVisit(id, cityVisitDTO);
         return ResponseEntity.ok(updatedCityVisit);
     }
 
-
+    @Operation(summary = "Delete a city visit")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCityVisit(@PathVariable Long id) {
         cityVisitFacade.deleteCityVisit(id);
