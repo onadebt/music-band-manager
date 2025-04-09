6a20dc2865fe6039a4fe402ceb5c4a3b7b25ddb1
Ivan <567817@muni.cz>
2025-04-09 18:01:16 +0200

    Controllers moved to rest package, added openapi annotations, added update endpoint

---
+++ user-service/src/main/java/cz/muni/fi/userservice/rest/ArtistController.java
@@ -0,0 +1,119 @@
+package cz.muni.fi.userservice.rest;
+
+import cz.muni.fi.userservice.dto.ArtistDto;
+import cz.muni.fi.userservice.dto.ArtistUpdateDto;
+import cz.muni.fi.userservice.facade.ArtistFacade;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/artists")
+@Tag(name = "Artist API", description = "Artist management API")
+public class ArtistController {
+
+    @Autowired
+    private ArtistFacade artistFacade;
+
+    @PostMapping
+    @Operation(summary = "Register artist", description = "Register a new artist"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Artist registered successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json")),
+    })
+    public ResponseEntity<ArtistDto> register(@Valid @RequestBody ArtistDto artistDTO) {
+        return ResponseEntity.ok(artistFacade.register(artistDTO));
+    }
+
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update artist by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Artist updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "404", description = "Artist not found", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+
+    })
+    public ResponseEntity<ArtistDto> update(@PathVariable Long id, @RequestBody ArtistUpdateDto artistUpdateDto) {
+        return ResponseEntity.ok(artistFacade.update(id, artistUpdateDto));
+    }
+
+
+    @GetMapping
+    @Operation(summary = "Get all artists")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of artists retrieved successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
+    public ResponseEntity<List<ArtistDto>> getAllArtists() {
+        return ResponseEntity.ok(artistFacade.findAll());
+    }
+
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get artist by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Artist found"),
+            @ApiResponse(responseCode = "404", description = "Artist not found", content = @Content(mediaType = "application/problem+json"))
+    })
+    public ResponseEntity<ArtistDto> getArtistById(@PathVariable Long id) {
+        return ResponseEntity.ok(artistFacade.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete artist by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Artist deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Artist not found", content = @Content(mediaType = "application/problem+json"))
+    })
+    public ResponseEntity<Void> deleteArtist(@PathVariable Long id) {
+        artistFacade.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/username/{username}")
+    @Operation(summary = "Get artist by username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Artist found"),
+            @ApiResponse(responseCode = "404", description = "Artist not found", content = @Content(mediaType = "application/problem+json"))
+    })
+    public ResponseEntity<ArtistDto> getArtistByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(artistFacade.findByUsername(username));
+    }
+
+    @PostMapping("/bands/{artistId}")
+    @Operation(summary = "Update artist's band IDs")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Artist's bands updated"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "404", description = "Artist not found", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
+    public ResponseEntity<ArtistDto> updateBands(@PathVariable Long artistId, @RequestBody Set<Long> bandIds) {
+        return ResponseEntity.ok(artistFacade.updateBandIds(artistId, bandIds));
+    }
+
+    @GetMapping("/bands")
+    @Operation(summary = "Get artists by band IDs")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of artists retrieved successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid band IDs provided", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
+    public ResponseEntity<List<ArtistDto>> getArtistsByBandIds(@RequestParam Set<Long> bandIds) {
+        return ResponseEntity.ok(artistFacade.findByBandIds(bandIds));
+    }
+
+}
---
+++ user-service/src/main/java/cz/muni/fi/userservice/rest/ManagerController.java
@@ -0,0 +1,102 @@
+package cz.muni.fi.userservice.rest;
+
+import cz.muni.fi.userservice.dto.ManagerDto;
+import cz.muni.fi.userservice.dto.ManagerUpdateDto;
+import cz.muni.fi.userservice.facade.ManagerFacade;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/managers")
+@Tag(name = "Manager API", description = "Manager management API")
+public class ManagerController {
+    @Autowired
+    private ManagerFacade managerFacade;
+
+    @PostMapping
+    @Operation(summary = "Register a new manager")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Manager registered successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json")),
+    })
+    public ResponseEntity<ManagerDto> register(@Valid @RequestBody ManagerDto managerDTO) {
+        return new ResponseEntity<>(managerFacade.register(managerDTO), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Get all managers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Manager deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Manager not found", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        managerFacade.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get manager by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Manager found"),
+            @ApiResponse(responseCode = "404", description = "Manager not found", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
+    public ResponseEntity<ManagerDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(managerFacade.findById(id));
+    }
+
+    @GetMapping
+    @Operation(summary = "Get all managers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of managers retrieved successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
+    public ResponseEntity<List<ManagerDto>> getAllManagers() {
+        return ResponseEntity.ok(managerFacade.findAll());
+    }
+
+    @PostMapping("/bands/{artistId}")
+    @Operation(summary = "Get manager by username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Manager bands updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Manager not found", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
+    public ResponseEntity<ManagerDto> updateBands(@PathVariable Long artistId, @RequestBody Set<Long> bandIds) {
+        return ResponseEntity.ok(managerFacade.updateBandIds(artistId, bandIds));
+    }
+
+    @GetMapping("/bands")
+    @Operation(summary = "Get managers by band IDs")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of managers by band IDs retrieved successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
+    public ResponseEntity<List<ManagerDto>> getManagersByBandIds(@RequestParam Set<Long> bandIds) {
+        return ResponseEntity.ok(managerFacade.findByBandIds(bandIds));
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update manager by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Manager updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Manager not found", content = @Content(mediaType = "application/problem+json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
+    public ResponseEntity<ManagerDto> updateManager(@PathVariable Long id, @RequestBody ManagerUpdateDto updateDto) {
+        return ResponseEntity.ok(managerFacade.update(id, updateDto));
+    }
+}
