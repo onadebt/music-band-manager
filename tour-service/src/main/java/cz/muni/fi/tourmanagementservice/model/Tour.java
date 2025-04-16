2e1d9cc68d234e834d2aa3b9bbe6861f8b39f55a
Zbyněk Pospíšil <zp.pospisil@seznam.cz>
2025-04-16 11:22:52 +0200

    Adding missing hashCode, equals and toString

--- tour-service/src/main/java/cz/muni/fi/tourmanagementservice/model/Tour.java
+++ tour-service/src/main/java/cz/muni/fi/tourmanagementservice/model/Tour.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 
 @Getter
 @Setter
@@ -24,5 +26,26 @@ public class Tour {
     private String tourName;
 
     public Tour() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Tour tour = (Tour) o;
+        return Objects.equals(id, tour.id) && Objects.equals(bandId, tour.bandId) && Objects.equals(tourName, tour.tourName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, bandId, tourName);
+    }
+
+    @Override
+    public String toString() {
+        return "Tour{" +
+                "id=" + id +
+                ", bandId=" + bandId +
+                ", tourName='" + tourName + '\'' +
+                '}';
     }
 }
