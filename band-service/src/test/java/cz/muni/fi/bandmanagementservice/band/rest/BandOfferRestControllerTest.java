c8cf75686628c5bc017ad5c1213ffd91b3756d7b
Ivan <567817@muni.cz>
2025-04-22 11:31:23 +0200

    added tests for BandOffer services and facade

--- band-service/src/test/java/cz/muni/fi/bandmanagementservice/band/rest/BandOfferRestControllerTest.java
+++ band-service/src/test/java/cz/muni/fi/bandmanagementservice/band/rest/BandOfferRestControllerTest.java
@@ -1,16 +1,13 @@
-package cz.muni.fi.bandmanagementservice.api;
+package cz.muni.fi.bandmanagementservice.band.rest;
 
 import cz.muni.fi.bandmanagementservice.band.dto.BandOfferDto;
 import cz.muni.fi.bandmanagementservice.band.exceptions.InvalidOperationException;
 import cz.muni.fi.bandmanagementservice.band.exceptions.ResourceNotFoundException;
 import cz.muni.fi.bandmanagementservice.band.facade.BandOfferFacade;
-import cz.muni.fi.bandmanagementservice.band.rest.BandOfferRestController;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
