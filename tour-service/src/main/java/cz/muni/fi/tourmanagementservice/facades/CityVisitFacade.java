4e64dfc0d1862e60307fec1b4b43949841a85f0e
Zbyněk Pospíšil <zp.pospisil@seznam.cz>
2025-04-23 13:42:23 +0200

    Changing cityVisits to mappers

--- tour-service/src/main/java/cz/muni/fi/tourmanagementservice/facades/CityVisitFacade.java
+++ tour-service/src/main/java/cz/muni/fi/tourmanagementservice/facades/CityVisitFacade.java
@@ -2,54 +2,83 @@ package cz.muni.fi.tourmanagementservice.facades;
 
 
 import cz.muni.fi.tourmanagementservice.dto.CityVisitDTO;
-import cz.muni.fi.tourmanagementservice.exception.ResourceNotFoundException;
+import cz.muni.fi.tourmanagementservice.mapper.CityVisitMapper;
+import cz.muni.fi.tourmanagementservice.mapper.TourMapper;
+import cz.muni.fi.tourmanagementservice.model.CityVisit;
+import cz.muni.fi.tourmanagementservice.model.Tour;
 import cz.muni.fi.tourmanagementservice.service.CityVisitService;
-import cz.muni.fi.tourmanagementservice.service.CityVisitService;
+import cz.muni.fi.tourmanagementservice.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CityVisitFacade {
 
     private final CityVisitService cityVisitService;
+    private final CityVisitMapper cityVisitMapper;
+    private final TourService tourService;
+    private final TourMapper tourMapper;
 
     @Autowired
-    public CityVisitFacade(CityVisitService cityVisitService) {
+    public CityVisitFacade(CityVisitService cityVisitService, CityVisitMapper cityVisitMapper, TourService tourService, TourMapper tourMapper) {
         this.cityVisitService = cityVisitService;
+        this.cityVisitMapper = cityVisitMapper;
+        this.tourService = tourService;
+        this.tourMapper = tourMapper;
     }
 
     public List<CityVisitDTO> getAllCityVisits() {
-        return cityVisitService.getAllCityVisits();
+        return cityVisitService.getAllCityVisits().stream()
+                .map(cityVisitMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     public List<CityVisitDTO> getCityVisitsByTour(Long tourId) {
-        return cityVisitService.getCityVisitByTour(tourId);
+        return cityVisitService.getCityVisitByTour(tourId).stream()
+                .map(cityVisitMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     public CityVisitDTO getCityVisitById(Long id) {
-        return cityVisitService.getCityVisitById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("CityVisit not found with id: " + id));
+        CityVisit cityVisit = cityVisitService.getCityVisitById(id);
+        return cityVisitMapper.toDTO(cityVisit);
     }
 
-    public CityVisitDTO createCityVisit(CityVisitDTO tourDTO) {
-        return cityVisitService.createCityVisit(tourDTO);
-    }
+    public CityVisitDTO createCityVisit(CityVisitDTO cityVisitDTO) {
+        CityVisit cityVisit = cityVisitMapper.toEntity(cityVisitDTO);
 
-    public CityVisitDTO updateCityVisit(Long id, CityVisitDTO tourDTO) {
-        CityVisitDTO updatedCityVisit = cityVisitService.updateCityVisit(id, tourDTO);
-        if (updatedCityVisit == null) {
-            throw new ResourceNotFoundException("CityVisit not found with id: " + id);
+        if (cityVisitDTO.getTourId() != null) {
+            Tour tour = tourService.getTourById(cityVisitDTO.getTourId());
+            cityVisit.setTour(tour);
         }
-        return updatedCityVisit;
+
+        CityVisit savedCityVisit = cityVisitService.createCityVisit(cityVisit);
+        return cityVisitMapper.toDTO(savedCityVisit);
     }
+
+
+    public CityVisitDTO updateCityVisit(Long id, CityVisitDTO cityVisitDTO) {
+        CityVisit cityVisit = cityVisitService.getCityVisitById(id);
+        cityVisitMapper.updateEntityFromDto(cityVisitDTO, cityVisit);
+
+        if (cityVisitDTO.getTourId() != null) {
+            Tour tour = tourService.getTourById(cityVisitDTO.getTourId());
+            cityVisit.setTour(tour);
+        }
+        else {
+            cityVisit.setTour(null);
+        }
+
+        CityVisit updatedCityVisit = cityVisitService.createCityVisit(cityVisit);
+        return cityVisitMapper.toDTO(updatedCityVisit);
+    }
+
 
     public void deleteCityVisit(Long id) {
-        boolean deleted = cityVisitService.deleteCityVisit(id);
-        if (!deleted) {
-            throw new ResourceNotFoundException("CityVisit not found with id: " + id);
-        }
+        cityVisitService.deleteCityVisit(id);
     }
 }
 
