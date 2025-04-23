e23b315b941ab267db0ac2c31f47caae79f5b27e
Ivan <567817@muni.cz>
2025-04-24 00:19:47 +0200

    fixed warnings with serialization

--- shared/src/main/java/cz/muni/fi/events/band/BandAddMemberEvent.java
+++ shared/src/main/java/cz/muni/fi/events/band/BandAddMemberEvent.java
@@ -2,6 +2,7 @@ package cz.muni.fi.events.band;
 
 import lombok.*;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Builder
@@ -11,6 +12,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BandAddMemberEvent implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private Long bandId;
