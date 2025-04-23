e8a8ea2a5ca66566da6af51f3a9cce407e290cdf
Zbyněk Pospíšil <zp.pospisil@seznam.cz>
2025-04-23 13:41:45 +0200

    Fixing mapper

--- tour-service/src/main/java/cz/muni/fi/tourmanagementservice/mapper/CityVisitMapper.java
+++ tour-service/src/main/java/cz/muni/fi/tourmanagementservice/mapper/CityVisitMapper.java
@@ -3,10 +3,12 @@ package cz.muni.fi.tourmanagementservice.mapper;
 import cz.muni.fi.tourmanagementservice.dto.CityVisitDTO;
 import cz.muni.fi.tourmanagementservice.model.CityVisit;
 import cz.muni.fi.tourmanagementservice.model.Tour;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 
+@Mapper(componentModel = "spring")
 public interface CityVisitMapper {
 
     @Mapping(source = "tour", target = "tourId", qualifiedByName = "tourToTourId")
