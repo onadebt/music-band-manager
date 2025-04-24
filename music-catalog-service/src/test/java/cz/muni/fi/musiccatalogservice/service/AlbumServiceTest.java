e5ce0226a544e84cdc9fd1f35f934a7667037e1f
Zbyněk Pospíšil <zp.pospisil@seznam.cz>
2025-04-24 03:19:03 +0200

    Adding music-catalog service tests

---
+++ music-catalog-service/src/test/java/cz/muni/fi/musiccatalogservice/service/AlbumServiceTest.java
@@ -0,0 +1,99 @@
+package cz.muni.fi.musiccatalogservice.service;
+
+import cz.muni.fi.musiccatalogservice.TestDataFactory;
+import cz.muni.fi.musiccatalogservice.model.Album;
+import cz.muni.fi.musiccatalogservice.repository.AlbumRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+@ExtendWith(MockitoExtension.class)
+public class AlbumServiceTest {
+
+    @Mock
+    AlbumRepository albumRepository;
+
+    @InjectMocks
+    AlbumService albumService;
+
+    @Test
+    void findAll_noAlbumsStored_returnsEmptyList() {
+        // Arrange
+        Mockito.when(albumRepository.findAll()).thenReturn(List.of());
+
+        // Act
+        List<Album> found = albumService.getAllAlbums();
+
+        // Assert
+        assertEquals(0, found.size());
+    }
+
+    @Test
+    void findAll_twoSongsStored_returnsList() {
+        // Arrange
+        Mockito.when(albumRepository.findAll()).thenReturn(List.of(TestDataFactory.TEST_ALBUM_1, TestDataFactory.TEST_ALBUM_2));
+
+        // Act
+        List<Album> found = albumService.getAllAlbums();
+
+        // Assert
+        assertEquals(2, found.size());
+        assertTrue(found.contains(TestDataFactory.TEST_ALBUM_1));
+        assertTrue(found.contains(TestDataFactory.TEST_ALBUM_2));
+    }
+
+    @Test
+    void deleteById_albumPresent_noException() {
+        // Arrange
+        Mockito.when(albumRepository.findById(TestDataFactory.TEST_ALBUM_1.getId()))
+                .thenReturn(Optional.of(TestDataFactory.TEST_ALBUM_1));
+
+        // Act
+        albumService.deleteAlbum(TestDataFactory.TEST_ALBUM_1.getId());
+
+        // Assert
+        Mockito.verify(albumRepository, Mockito.times(1)).findById(TestDataFactory.TEST_ALBUM_1.getId());
+        Mockito.verify(albumRepository, Mockito.times(1)).deleteById(TestDataFactory.TEST_ALBUM_1.getId());
+    }
+
+    @Test
+    void findByBandId_noAlbumInBand_returnsEmptyList() {
+        // Arrange
+        Long emptyBandId = 5L;
+        Mockito.when(albumRepository.findByBandId(emptyBandId)).thenReturn(List.of());
+
+        // Act
+        List<Album> found = albumService.getAlbumsByBand(emptyBandId);
+
+        // Assert
+        assertEquals(0, found.size());
+    }
+
+
+    @Test
+    void findByBandIds_albumsInBands_returnsList() {
+        // Arrange
+        Long bandId = 2L;
+        Mockito.when(albumRepository.findByBandId(bandId)).thenReturn(List.of(TestDataFactory.TEST_ALBUM_1, TestDataFactory.TEST_ALBUM_2));
+
+        // Act
+        List<Album> found = albumService.getAlbumsByBand(bandId);
+
+        // Assert
+        assertEquals(2, found.size());
+        assertTrue(found.contains(TestDataFactory.TEST_ALBUM_1));
+        assertTrue(found.contains(TestDataFactory.TEST_ALBUM_2));
+    }
+
+}
---
+++ music-catalog-service/src/test/java/cz/muni/fi/musiccatalogservice/service/SongServiceTest.java
@@ -0,0 +1,97 @@
+package cz.muni.fi.musiccatalogservice.service;
+
+import cz.muni.fi.musiccatalogservice.TestDataFactory;
+import cz.muni.fi.musiccatalogservice.model.Song;
+import cz.muni.fi.musiccatalogservice.repository.SongRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+public class SongServiceTest {
+
+    @Mock
+    SongRepository songRepository;
+
+    @InjectMocks
+    SongService songService;
+
+    @Test
+    void findAll_noSongsStored_returnsEmptyList() {
+        // Arrange
+        Mockito.when(songRepository.findAll()).thenReturn(List.of());
+
+        // Act
+        List<Song> found = songService.getAllSongs();
+
+        // Assert
+        assertEquals(0, found.size());
+    }
+
+    @Test
+    void findAll_twoSongsStored_returnsList() {
+        // Arrange
+        Mockito.when(songRepository.findAll()).thenReturn(List.of(TestDataFactory.TEST_SONG_1, TestDataFactory.TEST_SONG_2));
+
+        // Act
+        List<Song> found = songService.getAllSongs();
+
+        // Assert
+        assertEquals(2, found.size());
+        assertTrue(found.contains(TestDataFactory.TEST_SONG_1));
+        assertTrue(found.contains(TestDataFactory.TEST_SONG_2));
+    }
+
+    @Test
+    void deleteById_songPresent_noException() {
+        // Arrange
+        Mockito.when(songRepository.findById(TestDataFactory.TEST_SONG_1.getId()))
+                .thenReturn(Optional.of(TestDataFactory.TEST_SONG_1));
+
+        // Act
+        songService.deleteSong(TestDataFactory.TEST_SONG_1.getId());
+
+        // Assert
+        Mockito.verify(songRepository, Mockito.times(1)).findById(TestDataFactory.TEST_SONG_1.getId());
+        Mockito.verify(songRepository, Mockito.times(1)).deleteById(TestDataFactory.TEST_SONG_1.getId());
+    }
+
+    @Test
+    void findByAlbumIds_noSongInAlbums_returnsEmptyList() {
+        // Arrange
+        Long emptySongId = 5L;
+        Mockito.when(songRepository.findByAlbumId(emptySongId)).thenReturn(List.of());
+
+        // Act
+        List<Song> found = songService.getSongsByAlbum(emptySongId);
+
+        // Assert
+        assertEquals(0, found.size());
+    }
+
+
+    @Test
+    void findByAlbumIds_songsInAlbums_returnsList() {
+        // Arrange
+        Long albumId = 2L;
+        Mockito.when(songRepository.findByAlbumId(albumId)).thenReturn(List.of(TestDataFactory.TEST_SONG_1, TestDataFactory.TEST_SONG_2));
+
+        // Act
+        List<Song> found = songService.getSongsByAlbum(albumId);
+
+        // Assert
+        assertEquals(2, found.size());
+        assertTrue(found.contains(TestDataFactory.TEST_SONG_1));
+        assertTrue(found.contains(TestDataFactory.TEST_SONG_2));
+    }
+
+}
