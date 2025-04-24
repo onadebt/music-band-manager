dfd625f4c163ac4a95a4f98d8d81dcade6eb17af
Zbyněk Pospíšil <zp.pospisil@seznam.cz>
2025-04-24 04:34:57 +0200

    TourTesting init

---
+++ tour-service/src/test/java/cz/muni/fi/tourmanagementservice/facades/CityVisitFacadeTest.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.tourmanagementservice.facades;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityVisitFacadeTest {
+
+    @Test
+    void test() {
+
+    }
+}
