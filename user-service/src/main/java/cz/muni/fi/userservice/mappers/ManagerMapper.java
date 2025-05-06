ef55166af51a5b576ec838af4b17c23bc1e7c2e5
Ivan <567817@muni.cz>
2025-05-06 23:43:31 +0200

    ManagerMapper fix

--- user-service/src/main/java/cz/muni/fi/userservice/mappers/ManagerMapper.java
+++ user-service/src/main/java/cz/muni/fi/userservice/mappers/ManagerMapper.java
@@ -14,9 +14,21 @@ public interface ManagerMapper {
     ManagerDto toDto(Manager manager);
 
     default Manager updateManagerFromDto(ManagerUpdateDto updateDto, @MappingTarget Manager manager) {
-        UserMapper userMapper = Mappers.getMapper(UserMapper.class);
-        userMapper.updateUserFromDto(updateDto, manager);
-
+        if (updateDto.getUsername() != null) {
+            manager.setUsername(updateDto.getUsername());
+        }
+        if (updateDto.getPassword() != null) {
+            manager.setPassword(updateDto.getPassword());
+        }
+        if (updateDto.getEmail() != null) {
+            manager.setEmail(updateDto.getEmail());
+        }
+        if (updateDto.getFirstName() != null) {
+            manager.setFirstName(updateDto.getFirstName());
+        }
+        if (updateDto.getLastName() != null) {
+            manager.setLastName(updateDto.getLastName());
+        }
         if (updateDto.getCompanyName() != null) {
             manager.setCompanyName(updateDto.getCompanyName());
         }
