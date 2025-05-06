ee918ee9c99d18c81695e78541c4bf656b1abc0c
Ivan <567817@muni.cz>
2025-05-07 00:59:42 +0200

    fix precise Query added ManagerRepository

--- user-service/src/main/java/cz/muni/fi/userservice/repository/ManagerRepository.java
+++ user-service/src/main/java/cz/muni/fi/userservice/repository/ManagerRepository.java
@@ -3,6 +3,7 @@ package cz.muni.fi.userservice.repository;
 
 import cz.muni.fi.userservice.model.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,6 @@ import java.util.Set;
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
     Optional<Manager> findByUsername(String username);
 
+    @Query("SELECT m FROM Manager m JOIN m.managedBandIds mb WHERE mb IN :bandIds")
     List<Manager> findByManagedBandIds (Set<Long> bandIds);
 }
